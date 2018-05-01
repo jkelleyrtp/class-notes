4032ddfb9e74880e3e5c22d57868067c6c4de515
Jonathan Kelley <jkelleyrtp@gmail.com>
2018-05-01 12:57:48 -0400

    Updated with categories

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,11 @@
-var store = []
+var store = [{
+        "title": "Bi357_notes",
+        "excerpt":"Working Individually… ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/BI357_Notes/",
+        "teaser":null},{
+        "title": "Ma456_notes",
+        "excerpt":"Hello world! ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/MA456_Notes/",
+        "teaser":null}]
